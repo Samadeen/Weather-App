e16bc1d2f692a4786cf1af69ccd7c1126d522b38
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-08-21 21:00:40 +0100

    Api Setup from  GeoDB cites

---
+++ src/api/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'ff4a3fe9f4mshc7743e07bf52c5ap14511djsn641ca3bc740d',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
