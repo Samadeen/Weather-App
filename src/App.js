dadbe6cebe378960f9cd476603b5b1ff5fd7d854
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-08-23 10:29:44 +0100

    Initiated Loading State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Search from './components/Search/Search';
 import CurrentWeather from './components/CurrentWeather/CurrentWeather';
+import Forecast from './components/Forecast/Forecast';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api/api';
 
 import './App.css';
@@ -8,9 +9,11 @@ import './App.css';
 function App() {
   const [currentWeather, setCurrentWeather] = useState();
   const [forecast, setForecast] = useState();
+  const [loading, setIsLoading] = useState(false);
 
   const onSearchChangeHandler = (searchData) => {
     const [lat, lon] = searchData.value.split(' ');
+    setIsLoading(true);
 
     const currentWeatherFetcher = fetch(
       `${WEATHER_API_URL}weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
@@ -24,6 +27,7 @@ function App() {
       const weatherRes = await res[0].json();
       const forecastRes = await res[1].json();
 
+      setIsLoading(false);
       setCurrentWeather({ city: searchData.label, ...weatherRes });
       setForecast({ city: searchData.label, ...forecastRes });
     });
@@ -34,7 +38,10 @@ function App() {
   return (
     <div className='container'>
       <Search onSearchChange={onSearchChangeHandler} />
-      {currentWeather && <CurrentWeather props={currentWeather} />}
+      {currentWeather && (
+        <CurrentWeather props={currentWeather} loading={loading} />
+      )}
+      {forecast && <Forecast props={forecast} />}
     </div>
   );
 }
--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,51 +1,57 @@
 import React from 'react';
+import Loading from '../../ui/Loading';
 import './CurrentWeather.css';
 
-const CurrentWeather = ({ props }) => {
+const CurrentWeather = ({ props, loading }) => {
   return (
-    <div className='weather'>
-      <div className='top'>
-        <div className='paragraphs'>
-          <p className='city'>{props.city}</p>
-          <p className='weather_description'>{props.weather[0].description}</p>
+    <>
+      {loading.loading && <Loading />}
+      <div className='weather'>
+        <div className='top'>
+          <div className='paragraphs'>
+            <p className='city'>{props.city}</p>
+            <p className='weather_description'>
+              {props.weather[0].description}
+            </p>
+          </div>
+          <img
+            alt='weather'
+            className='weather_icon'
+            src={`icons/${props.weather[0].icon}.png`}
+          />
         </div>
-        <img
-          alt='weather'
-          className='weather_icon'
-          src={`icons/${props.weather[0].icon}.png`}
-        />
-      </div>
-      <div className='bottom'>
-        <p className='temperature'>{Math.round(props.main.temp)} ºC</p>
-        <div className='details'>
-          <div className='parameter_row'>
-            <span className='parameter-label'>Details</span>
-          </div>
-          <div className='parameter_row'>
-            <span className='parameter-label'>Feels Like</span>
-            <span className='parameter-value'>
-              {`${props.main.feels_like}ºC`}
-            </span>
-          </div>{' '}
-          <div className='parameter_row'>
-            <span className='parameter-label'>Wind</span>
-            <span className='parameter-value'>{`${props.wind.speed} m/s`}</span>
-          </div>
-          <div className='parameter_row'>
-            <span className='parameter-label'>Humidity</span>
-            <span className='parameter-value'>
-              {Math.round(props.main.humidity)} %
-            </span>
-          </div>
-          <div className='parameter_row'>
-            <span className='parameter-label'>Pressure</span>
-            <span className='parameter-value'>
-              {Math.round(props.main.pressure)} hPa
-            </span>
+        <div className='bottom'>
+          <p className='temperature'>{Math.round(props.main.temp)}°C</p>
+          <div className='details'>
+            <div className='parameter_row'>
+              <span className='parameter-label top'>Details</span>
+            </div>
+            <div className='parameter_row'>
+              <span className='parameter-label'>Feels Like</span>
+              <span className='parameter-value'>
+                {`${props.main.feels_like}°C`}
+              </span>
+            </div>{' '}
+            <div className='parameter_row'>
+              <span className='parameter-label'>Wind</span>
+              <span className='parameter-value'>{`${props.wind.speed} m/s`}</span>
+            </div>
+            <div className='parameter_row'>
+              <span className='parameter-label'>Humidity</span>
+              <span className='parameter-value'>
+                {Math.round(props.main.humidity)} %
+              </span>
+            </div>
+            <div className='parameter_row'>
+              <span className='parameter-label'>Pressure</span>
+              <span className='parameter-value'>
+                {Math.round(props.main.pressure)} hPa
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
